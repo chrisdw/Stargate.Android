48a3cd7e5533ff8bedb55aeaf70b378d12c12df3
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2013-09-10 16:03:37 +0100

    Upgrade to ViewPager
    Change from Tabs to a view pager

--- src/uk/org/downesward/stargate/CustomPagerAdapter.java
+++ src/uk/org/downesward/stargate/CustomPagerAdapter.java
@@ -8,16 +8,19 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CustomPagerAdapter extends PagerAdapter {
 	private Context mContext;
 	private Vector<View> pages;
-
+    private World world;
+    
 	List<String> pageTitles = new ArrayList<String>();
 
-	public CustomPagerAdapter(Context context, Vector<View> pages) {
+	public CustomPagerAdapter(Context context, Vector<View> pages, World world) {
 		this.mContext = context;
 		this.pages = pages;
+		this.setWorld(world);
 
 		pageTitles.add("Thermosphere");
 		pageTitles.add("Atmosphere");
@@ -30,7 +33,7 @@ public class CustomPagerAdapter extends PagerAdapter {
 		pageTitles.add("Military");
 		pageTitles.add("Government");
 		pageTitles.add("Xenosphere");
-		pageTitles.add("Origin");
+		pageTitles.add("Civil Rights");
 		pageTitles.add("Origin");
 		pageTitles.add("Notes");
 	}
@@ -38,6 +41,136 @@ public class CustomPagerAdapter extends PagerAdapter {
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
 		View page = pages.get(position);
+		if (world != null) {
+			switch (position) {
+			case 0:
+				// Thermosphere		
+	
+				setTextView(page, R.id.txtThermos, world.thermosphere.thermo);
+				setTextView(page, R.id.txtThermoCR, world.thermosphere.cr);
+				setTextView(page, R.id.txtOrbit, world.thermosphere.orbit);
+				setTextView(page, R.id.txtTemperature, world.thermosphere.temp);
+				break;
+				
+			case 1:
+				
+				// Atmosphere
+				setTextView(page, R.id.txtAtmosphere, world.atmosphere.atmosphere);
+				setTextView(page, R.id.txtAtmosCR, world.atmosphere.cr);
+				setTextView(page, R.id.txtPressure, world.atmosphere.pressure);
+				setTextView(page, R.id.txtIncrement, world.atmosphere.increment);
+				setTextView(page, R.id.txtSave, world.atmosphere.save);
+				setTextView(page, R.id.txtDamage, world.atmosphere.damage);
+				setTextView(page, R.id.txtPen, world.atmosphere.pen);
+				break;
+				
+			case 2:
+				// Hydrosphere
+				setTextView(page, R.id.txtHydrosphere, world.hydrosphere.hydrosphere);
+				setTextView(page, R.id.txtRain, world.hydrosphere.rain);
+				setTextView(page, R.id.txtNone, world.hydrosphere.none);
+				setTextView(page, R.id.txtFlood, world.hydrosphere.flood);
+				setTextView(page, R.id.txtStorm, world.hydrosphere.storm);
+				setTextView(page, R.id.txtWind, world.hydrosphere.wind);
+				break;
+				
+			case 3:
+				// Geosphere Basic
+				setTextView(page, R.id.txtGeoBasic, world.geospherebasic.geosphere);
+				setTextView(page, R.id.txtGeobSpecial, world.geospherebasic.special);
+				break;
+				
+			case 4:
+				// Geosphere Tectonic
+				setTextView(page, R.id.txtGeoTectonic, world.geospheretectonic.geosphere);
+				setTextView(page, R.id.txtQuakeNone, world.geospheretectonic.quakesnone);	
+				setTextView(page, R.id.txtQuakeMinor, world.geospheretectonic.quakesminor);
+				setTextView(page, R.id.txtQuakeModerate, world.geospheretectonic.quakesmoderate);
+				setTextView(page, R.id.txtQuakeMajor, world.geospheretectonic.quakesmajor);
+				
+				setTextView(page, R.id.txtVolcanoNone, world.geospheretectonic.volcanoesnone);
+				setTextView(page, R.id.txtVolcanoSmall, world.geospheretectonic.volcanoessmall);
+				setTextView(page, R.id.txtVolcanoMedium, world.geospheretectonic.volcanoesmedium);
+				setTextView(page, R.id.txtVolcanoLarge, world.geospheretectonic.volcanoeslarge);
+				break;
+				
+			case 5:
+				// Biosphere
+				setTextView(page, R.id.txtBiosphere, world.biosphere.biosphere);
+				setTextView(page, R.id.txtMove, world.biosphere.move);
+				setTextView(page, R.id.txtTemperature, world.biosphere.temperature);
+				setTextView(page, R.id.txtRain, world.biosphere.rain);
+				setTextView(page, R.id.txtVegetation, world.biosphere.vegetation);
+				break;	
+				
+			case 6:
+
+				// Seasons
+				setTextView(page, R.id.txtSeasons, world.seasons.seasons);		
+				setTextView(page, R.id.txtSpring, world.seasons.spring);		
+				setTextView(page, R.id.txtSpringTemp, world.seasons.springtemprature);		
+				setTextView(page, R.id.txtSummer, world.seasons.summer);		
+				setTextView(page, R.id.txtSummerTemp, world.seasons.summertemprature);		
+				setTextView(page, R.id.txtAutumn, world.seasons.autumn);		
+				setTextView(page, R.id.txtAutumnTemp, world.seasons.autumntemprature);
+				setTextView(page, R.id.txtWinter, world.seasons.winter);		
+				setTextView(page, R.id.txtWinterTemp, world.seasons.wintertemprature);
+				break;
+				
+			case 7:
+				// Anthrosphere
+				setTextView(page, R.id.txtAnthrosphere, world.anthrosphere.anthrosphere);
+				setTextView(page, R.id.txtAdvances, world.anthrosphere.advances);
+				setTextView(page, R.id.txtWeapons, world.anthrosphere.weapons);
+				setTextView(page, R.id.txtVehicles, world.anthrosphere.vehicles);
+				setTextView(page, R.id.txtPicks, world.anthrosphere.picks);
+				setTextView(page, R.id.txtRP, world.anthrosphere.rp);
+				break;	
+
+			case 8:
+				// Military
+				setTextView(page, R.id.txtMilitary, world.military.military);
+				setTextView(page, R.id.txtForce, world.military.force);
+				setTextView(page, R.id.txtSquadSize, world.military.squadsize);
+				setTextView(page, R.id.txtCondition, world.military.condition);
+				break;
+				
+			case 9:
+				// Government
+				setTextView(page, R.id.txtGovernment, world.government.government);
+				setTextView(page, R.id.txtRuler, world.government.ruler);
+				break;
+				
+			case 10:
+				// Xenophobia
+				setTextView(page, R.id.txtXenophobia, world.xenophobia.xenophobia);
+				setTextView(page, R.id.txtDisposition, world.xenophobia.disposition);
+				setTextView(page, R.id.txtModifier, world.xenophobia.modifier);
+				break;
+				
+			case 11:
+				// Civil Rights
+				setTextView(page, R.id.txtCivilRights, world.civilrights.civilrights);
+				setTextView(page, R.id.txtSubjagation, world.civilrights.subjagation);
+				setTextView(page, R.id.txtGenocide, world.civilrights.genocide);
+				break;
+				
+			case 12:
+				// Origin/Culture Age
+				setTextView(page, R.id.txtOriginCultureAge, world.origincultureage.origincultureage);
+				
+				// Origin/Culture Roots
+				setTextView(page, R.id.txtOriginCultureRoots, world.origincultureroots.origincultureroots);
+				setTextView(page, R.id.txtOriginCultureDescription, world.origincultureroots.description);
+				break;
+				
+			case 13:
+				
+				// Notes
+				setTextView(page, R.id.txtNotes, world.notes);
+				break;
+			}
+		}
 		container.addView(page);
 		return page;
 	}
@@ -60,5 +193,20 @@ public class CustomPagerAdapter extends PagerAdapter {
 	@Override
 	public CharSequence getPageTitle(int position) {
 		return pageTitles.get(position);
+	}
+	
+	private void setTextView(View page, int id, String value) {
+		TextView view = (TextView)page.findViewById(id);
+		if (view != null && value != null) {
+			view.setText(value);
+		}
+	}
+
+	public World getWorld() {
+		return world;
+	}
+
+	public void setWorld(World world) {
+		this.world = world;
 	}
 }
--- src/uk/org/downesward/stargate/WorldViewerActivity.java
+++ src/uk/org/downesward/stargate/WorldViewerActivity.java
@@ -5,140 +5,96 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.Reader;
+import java.util.List;
+import java.util.Vector;
 
+
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.TabActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TabHost;
+import android.view.View;
 import android.widget.TextView;
 
 import com.lamerman.FileDialog;
 import com.lamerman.SelectionMode;
 
-public class WorldViewerActivity extends TabActivity {
+public class WorldViewerActivity extends Activity {
 	
 	private static final int REQUEST_SAVE = 0;
 	private static final int REQUEST_OPEN = 1;
 	private static final int REQUEST_OPEN_KNOWN = 2;
 	private World world;
 	
+	 private Context mContext;
+	 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		try
 		{
-		setContentView(R.layout.worldviewer);
-
-		Resources res = getResources(); // Resource object to get Drawables
-		TabHost tabHost = getTabHost(); // The activity TabHost
-		TabHost.TabSpec spec; // Resusable TabSpec for each tab
-
-		// Initialize a TabSpec for each tab and add it to the TabHost
-		spec = tabHost
-				.newTabSpec("Thermosphere")
-				.setIndicator("Thermosphere",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabThermos);
-		tabHost.addTab(spec);
-
-		spec = tabHost
-				.newTabSpec("atmosphere")
-				.setIndicator("Atmosphere",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabAtmosphere);
-		tabHost.addTab(spec);
-		
-		spec = tabHost
-				.newTabSpec("hydrosphere")
-				.setIndicator("Hydrosphere",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabHydrosphere);
-		tabHost.addTab(spec);
-		
-		spec = tabHost
-				.newTabSpec("geospherebasic")
-				.setIndicator("Geosphere (Basic)",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabGeosphereBasic);
-		tabHost.addTab(spec);		
-		
-		spec = tabHost
-				.newTabSpec("geospheretectonic")
-				.setIndicator("Geosphere (Tectonic)",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabGeosphereTectonic);
-		tabHost.addTab(spec);
-		
-		spec = tabHost
-				.newTabSpec("biosphere")
-				.setIndicator("Biosphere",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabBiosphere);
-		tabHost.addTab(spec);
-		
-		spec = tabHost
-				.newTabSpec("seasons")
-				.setIndicator("Seasons",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabSeasons);
-		tabHost.addTab(spec);	
-		
-		spec = tabHost
-				.newTabSpec("anthrosphere")
-				.setIndicator("Anthrosphere",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabAnthrosphere);
-		tabHost.addTab(spec);		
-		
-		spec = tabHost
-				.newTabSpec("military")
-				.setIndicator("Military",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabMilitary);
-		tabHost.addTab(spec);
-		
-		spec = tabHost
-				.newTabSpec("government")
-				.setIndicator("Government",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabGovernment);
-		tabHost.addTab(spec);			
-		
-		spec = tabHost
-				.newTabSpec("xenophobia")
-				.setIndicator("Xenophobia",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabXenophobia);
-		tabHost.addTab(spec);	
-		
-		spec = tabHost
-				.newTabSpec("civilrights")
-				.setIndicator("Civil Rights",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabCivilRights);
-		tabHost.addTab(spec);	
-		
-		spec = tabHost
-				.newTabSpec("origin")
-				.setIndicator("Origin",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabOrigin);
-		tabHost.addTab(spec);		
-		
-		spec = tabHost
-				.newTabSpec("notes")
-				.setIndicator("Notes",
-						res.getDrawable(R.drawable.ic_launcher2))
-				.setContent(R.id.tabNotes);
-		tabHost.addTab(spec);			
+	        mContext = this;
+	        setContentView(R.layout.worldviewer);
+	        
+	        ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
+	        
+			Vector<View> pages = new Vector<View>();
+			
+			LayoutInflater inflater = LayoutInflater.from(mContext);
+			
+			View page = inflater.inflate(R.layout.thermosphere, null, false);
+			pages.add(page);
+			
+			page = inflater.inflate(R.layout.atmosphere, null, false);
+			pages.add(page);
+			
+			page = inflater.inflate(R.layout.hydrosphere, null, false);
+			pages.add(page);			
+			
+			page = inflater.inflate(R.layout.geospherebasic, null, false);
+			pages.add(page);	
+			
+			page = inflater.inflate(R.layout.geospheretectonic, null, false);
+			pages.add(page);			
+			
+			page = inflater.inflate(R.layout.biosphere, null, false);
+			pages.add(page);
+			
+			page = inflater.inflate(R.layout.seasons, null, false);
+			pages.add(page);
+			
+			page = inflater.inflate(R.layout.anthrosphere, null, false);
+			pages.add(page);	
+			
+			page = inflater.inflate(R.layout.military, null, false);
+			pages.add(page);				
+			
+			page = inflater.inflate(R.layout.government, null, false);
+			pages.add(page);	
+			
+			page = inflater.inflate(R.layout.xenophobia, null, false);
+			pages.add(page);	
+			
+			page = inflater.inflate(R.layout.civilrights, null, false);
+			pages.add(page);			
+			
+			page = inflater.inflate(R.layout.origin, null, false);
+			pages.add(page);		
+			
+			page = inflater.inflate(R.layout.notes, null, false);
+			pages.add(page);			
+			
+	        CustomPagerAdapter adapter = new CustomPagerAdapter(mContext,pages,world);
+	        vp.setAdapter(adapter);	
 		
 		Bundle b = getIntent().getExtras();
 		String worldFilename = "";
@@ -156,11 +112,11 @@ public class WorldViewerActivity extends TabActivity {
 				e.printStackTrace();
 			}
 		}
-		tabHost.setCurrentTab(0);
+		//tabHost.setCurrentTab(0);
 		}
 		catch (Throwable e) {
 			// Something is dying in real life - need to find out what.
-			AlertDialog.Builder builder = new AlertDialog.Builder(getCurrentActivity());
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(e.getLocalizedMessage())
 		       .setTitle(e.getMessage());
 			builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
@@ -205,100 +161,11 @@ public class WorldViewerActivity extends TabActivity {
 		world = new World(input);
 		this.setTitle(world.name);
 
-		// Thermosphere
-		setTextView(R.id.txtThermos, world.thermosphere.thermo);
-		setTextView(R.id.txtThermoCR, world.thermosphere.cr);
-		setTextView(R.id.txtOrbit, world.thermosphere.orbit);
-		setTextView(R.id.txtTemperature, world.thermosphere.temp);
-		
-		// Atmosphere
-		setTextView(R.id.txtAtmosphere, world.atmosphere.atmosphere);
-		setTextView(R.id.txtAtmosCR, world.atmosphere.cr);
-		setTextView(R.id.txtPressure, world.atmosphere.pressure);
-		setTextView(R.id.txtIncrement, world.atmosphere.increment);
-		setTextView(R.id.txtSave, world.atmosphere.save);
-		setTextView(R.id.txtDamage, world.atmosphere.damage);
-		setTextView(R.id.txtPen, world.atmosphere.pen);
-		
-		// Hydrosphere
-		setTextView(R.id.txtHydrosphere, world.hydrosphere.hydrosphere);
-		setTextView(R.id.txtRain, world.hydrosphere.rain);
-		setTextView(R.id.txtNone, world.hydrosphere.none);
-		setTextView(R.id.txtFlood, world.hydrosphere.flood);
-		setTextView(R.id.txtStorm, world.hydrosphere.storm);
-		setTextView(R.id.txtWind, world.hydrosphere.wind);
-		
-		// Geosphere Basic
-		setTextView(R.id.txtGeoBasic, world.geospherebasic.geosphere);
-		setTextView(R.id.txtGeobSpecial, world.geospherebasic.special);
-		
-		// Geosphere Tectonic
-		setTextView(R.id.txtGeoTectonic, world.geospheretectonic.geosphere);
-		setTextView(R.id.txtQuakeNone, world.geospheretectonic.quakesnone);	
-		setTextView(R.id.txtQuakeMinor, world.geospheretectonic.quakesminor);
-		setTextView(R.id.txtQuakeModerate, world.geospheretectonic.quakesmoderate);
-		setTextView(R.id.txtQuakeMajor, world.geospheretectonic.quakesmajor);
-		
-		setTextView(R.id.txtVolcanoNone, world.geospheretectonic.volcanoesnone);
-		setTextView(R.id.txtVolcanoSmall, world.geospheretectonic.volcanoessmall);
-		setTextView(R.id.txtVolcanoMedium, world.geospheretectonic.volcanoesmedium);
-		setTextView(R.id.txtVolcanoLarge, world.geospheretectonic.volcanoeslarge);
-		
-		// Biosphere
-		setTextView(R.id.txtBiosphere, world.biosphere.biosphere);
-		setTextView(R.id.txtMove, world.biosphere.move);
-		setTextView(R.id.txtTemperature, world.biosphere.temperature);
-		setTextView(R.id.txtRain, world.biosphere.rain);
-		setTextView(R.id.txtVegetation, world.biosphere.vegetation);
-		
-		// Seasons
-		setTextView(R.id.txtSeasons, world.seasons.seasons);		
-		setTextView(R.id.txtSpring, world.seasons.spring);		
-		setTextView(R.id.txtSpringTemp, world.seasons.springtemprature);		
-		setTextView(R.id.txtSummer, world.seasons.summer);		
-		setTextView(R.id.txtSummerTemp, world.seasons.summertemprature);		
-		setTextView(R.id.txtAutumn, world.seasons.autumn);		
-		setTextView(R.id.txtAutumnTemp, world.seasons.autumntemprature);
-		setTextView(R.id.txtWinter, world.seasons.winter);		
-		setTextView(R.id.txtWinterTemp, world.seasons.wintertemprature);
-		
-		// Anthrosphere
-		setTextView(R.id.txtAnthrosphere, world.anthrosphere.anthrosphere);
-		setTextView(R.id.txtAdvances, world.anthrosphere.advances);
-		setTextView(R.id.txtWeapons, world.anthrosphere.weapons);
-		setTextView(R.id.txtVehicles, world.anthrosphere.vehicles);
-		setTextView(R.id.txtPicks, world.anthrosphere.picks);
-		setTextView(R.id.txtRP, world.anthrosphere.rp);
-		
-		// Military
-		setTextView(R.id.txtMilitary, world.military.military);
-		setTextView(R.id.txtForce, world.military.force);
-		setTextView(R.id.txtSquadSize, world.military.squadsize);
-		setTextView(R.id.txtCondition, world.military.condition);
-		
-		// Government
-		setTextView(R.id.txtGovernment, world.government.government);
-		setTextView(R.id.txtRuler, world.government.ruler);
-		
-		// Xenophobia
-		setTextView(R.id.txtXenophobia, world.xenophobia.xenophobia);
-		setTextView(R.id.txtDisposition, world.xenophobia.disposition);
-		setTextView(R.id.txtModifier, world.xenophobia.modifier);
-		
-		// Civil Rights
-		setTextView(R.id.txtCivilRights, world.civilrights.civilrights);
-		setTextView(R.id.txtSubjagation, world.civilrights.subjagation);
-		setTextView(R.id.txtGenocide, world.civilrights.genocide);
-		
-		// Origin/Culture Age
-		setTextView(R.id.txtOriginCultureAge, world.origincultureage.origincultureage);
-		
-		// Origin/Culture Roots
-		setTextView(R.id.txtOriginCultureRoots, world.origincultureroots.origincultureroots);
-		setTextView(R.id.txtOriginCultureDescription, world.origincultureroots.description);
-		
-		// Notes
-		setTextView(R.id.txtNotes, world.notes);
+		// Notify change
+		ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
+		CustomPagerAdapter adapter = (CustomPagerAdapter)vp.getAdapter();
+		adapter.setWorld(world);
+		adapter.notifyDataSetChanged();
 	}
 	
 	@Override
@@ -320,14 +187,7 @@ public class WorldViewerActivity extends TabActivity {
 		}
 		return true;
 	}	
-	
-	private void setTextView(int id, String value) {
-		TextView view = (TextView)this.findViewById(id);
-		if (view != null && value != null) {
-			view.setText(value);
-		}
-	}
-	
+
 	private void getWorldFile() {
 		Intent intent = new Intent(this.getBaseContext(), FileDialog.class);
 		intent.putExtra(FileDialog.START_PATH,
