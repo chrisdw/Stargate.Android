718647ee4eb952ab857da842c458b07715a55454
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2013-09-12 10:39:44 +0100

    Add rest of plot points

--- src/uk/org/downesward/stargate/DatabaseHelper.java
+++ src/uk/org/downesward/stargate/DatabaseHelper.java
@@ -1,41 +1,74 @@
 package uk.org.downesward.stargate;
 
-
-
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-
-
 public class DatabaseHelper extends SQLiteAssetHelper {
 
 	private static String DB_NAME = "worldgen";
 
-
 	public DatabaseHelper(Context context) {
 		super(context, DB_NAME, null, 1);
-
 	}
 
 	public Cursor getHooks() {
 		SQLiteDatabase db = getReadableDatabase();
-		Cursor res = db.rawQuery("SELECT BaseId, Hook, Description FROM Hook", null);
+		Cursor res = db.rawQuery("SELECT BaseId, Hook, Description FROM Hook",
+				null);
 		return res;
-
 	}
 
 	public Cursor getGoals() {
 		SQLiteDatabase db = getReadableDatabase();
-		Cursor res = db.rawQuery("SELECT BaseId, Goal, Description FROM Goal", null);
+		Cursor res = db.rawQuery("SELECT BaseId, Goal, Description FROM Goal",
+				null);
 		return res;
 	}
-	
+
 	public Cursor getPlots() {
 		SQLiteDatabase db = getReadableDatabase();
-		Cursor res = db.rawQuery("SELECT BaseId, PlotType, Description FROM PlotType", null);
+		Cursor res = db.rawQuery(
+				"SELECT BaseId, PlotType, Description FROM PlotType", null);
 		return res;
-	}	
+	}
+
+	public Cursor getClimaxes() {
+		SQLiteDatabase db = getReadableDatabase();
+		Cursor res = db.rawQuery(
+				"SELECT BaseId, Climax, Description FROM Climax", null);
+		return res;
+	}
+
+	public Cursor getEncounters() {
+		SQLiteDatabase db = getReadableDatabase();
+		Cursor res = db.rawQuery(
+				"SELECT BaseId, Encounter, Description FROM Encounters", null);
+		return res;
+	}
+
+	public Cursor getLocation() {
+		SQLiteDatabase db = getReadableDatabase();
+		Cursor res = db.rawQuery(
+				"SELECT BaseId, Location, Description FROM Location", null);
+		return res;
+	}
+
+	public Cursor getNPCs() {
+		SQLiteDatabase db = getReadableDatabase();
+		Cursor res = db
+				.rawQuery(
+						"SELECT BaseId, NPC, Description, RelativeLevel, Reaction FROM NPC",
+						null);
+		return res;
+	}
+
+	public Cursor getSettings() {
+		SQLiteDatabase db = getReadableDatabase();
+		Cursor res = db.rawQuery(
+				"SELECT BaseId, Setting, Description FROM Setting", null);
+		return res;
+	}
 }
--- src/uk/org/downesward/stargate/PlotGeneratorActivity.java
+++ src/uk/org/downesward/stargate/PlotGeneratorActivity.java
@@ -4,8 +4,8 @@ import uk.org.downesward.utiliites.Dice;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,24 +13,24 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class PlotGeneratorActivity extends Activity {
-	
+
 	private DatabaseHelper db;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.plotgenerator);
-		
-		db = new DatabaseHelper(this); 
+
+		db = new DatabaseHelper(this);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.plotgenerator, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int itemId = item.getItemId();
@@ -41,12 +41,11 @@ public class PlotGeneratorActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 		return true;
-	}	
-	
+	}
+
 	private void generatePlot() {
-		try
-		{
-			
+		try {
+
 			// Hook
 			Cursor res = db.getHooks();
 			Dice die = new Dice(res.getCount());
@@ -57,7 +56,7 @@ public class PlotGeneratorActivity extends Activity {
 				tv = (TextView) this.findViewById(R.id.txtHookDesc);
 				tv.setText(res.getString(2));
 			}
-			
+
 			// Goal
 			res = db.getGoals();
 			die = new Dice(res.getCount());
@@ -68,7 +67,7 @@ public class PlotGeneratorActivity extends Activity {
 				tv = (TextView) this.findViewById(R.id.txtGoalDesc);
 				tv.setText(res.getString(2));
 			}
-			
+
 			// Plot type
 			res = db.getPlots();
 			die = new Dice(res.getCount());
@@ -79,35 +78,81 @@ public class PlotGeneratorActivity extends Activity {
 				tv = (TextView) this.findViewById(R.id.txtPlotTypeDesc);
 				tv.setText(res.getString(2));
 			}
-			
-			// Setting
-			
-			// NPC
-			
-			// Location
-			
-			// Encounters
-			
-			// Climax
 
-		}
-		catch (Throwable e) {
+			// Setting
+			res = db.getSettings();
+			die = new Dice(res.getCount());
+			row = die.roll();
+			if (res.moveToPosition(row)) {
+				TextView tv = (TextView) this.findViewById(R.id.txtSetting);
+				tv.setText(res.getString(1));
+				tv = (TextView) this.findViewById(R.id.txtSettingDesc);
+				tv.setText(res.getString(2));
+			}
+			// NPC
+			res = db.getNPCs();
+			die = new Dice(res.getCount());
+			row = die.roll();
+			if (res.moveToPosition(row)) {
+				TextView tv = (TextView) this.findViewById(R.id.txtNPC);
+				tv.setText(res.getString(1));
+				tv = (TextView) this.findViewById(R.id.txtNPCDesc);
+				tv.setText(res.getString(2));
+				tv = (TextView) this.findViewById(R.id.txtNPCRelativeLevel);
+				tv.setText(res.getString(3));
+				tv = (TextView) this.findViewById(R.id.txtNPCReaction);
+				tv.setText(res.getString(4));
+			}
+			// Location
+			res = db.getLocation();
+			die = new Dice(res.getCount());
+			row = die.roll();
+			if (res.moveToPosition(row)) {
+				TextView tv = (TextView) this.findViewById(R.id.txtLocation);
+				tv.setText(res.getString(1));
+				tv = (TextView) this.findViewById(R.id.txtLocationDesc);
+				tv.setText(res.getString(2));
+			}
+			// Encounters
+			res = db.getEncounters();
+			die = new Dice(res.getCount());
+			row = die.roll();
+			if (res.moveToPosition(row)) {
+				TextView tv = (TextView) this.findViewById(R.id.txtEncounters);
+				tv.setText(res.getString(1));
+				tv = (TextView) this.findViewById(R.id.txtEncountersDesc);
+				tv.setText(res.getString(2));
+			}
+			// Climax
+			res = db.getClimaxes();
+			die = new Dice(res.getCount());
+			row = die.roll();
+			if (res.moveToPosition(row)) {
+				TextView tv = (TextView) this.findViewById(R.id.txtClimax);
+				tv.setText(res.getString(1));
+				tv = (TextView) this.findViewById(R.id.txtClimaxDesc);
+				tv.setText(res.getString(2));
+			}
+
+		} catch (Throwable e) {
 			// Something is dying in real life - need to find out what.
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(e.getLocalizedMessage())
-		       .setTitle("Exception");
-			builder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog,int id) {
-					// if this button is clicked, close
-					// current activity
-					PlotGeneratorActivity.this.finish();
-				}
-			  });		
+			Resources res = getResources();
+			
+			builder.setMessage(e.getLocalizedMessage()).setTitle(res.getString(R.string.val_exception));
+			builder.setPositiveButton(res.getString(R.string.val_ok),
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							// if this button is clicked, close
+							// current activity
+							PlotGeneratorActivity.this.finish();
+						}
+					});
 			AlertDialog dialog = builder.create();
 			dialog.show();
 		}
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
